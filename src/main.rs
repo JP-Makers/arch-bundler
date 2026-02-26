2442359f8e2fa35ff3f72bc9f7fa25ec1b233a46
magiyaazh <jeyaraj@chaos.jp-makers.com>
2026-02-26 09:45:41 +0530

    metadata

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use linux_bundler::rpm_build;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     linux_bundler::build_package().await?;
     alpm_build()?;
-    //appimage_build()?;
+    appimage_build()?;
     deb_build()?;
     rpm_build()?;
     Ok(())
